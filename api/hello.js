976146962f27d121c1784a150f9a56ce046e76fd
mgreen216 <mgreen216@users.noreply.github.com>
2025-05-02 17:59:06 -0400

    Delete api directory

--- api/hello.js
+++
@@ -1,5 +0,0 @@
-// api/hello.js
-export default function handler(request, response) {
-  console.log("--- Node.js hello function invoked ---");
-  response.status(200).json({ message: 'Hello from Node!' });
-}
